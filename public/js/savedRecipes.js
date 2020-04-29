32462ca04c98af90acfdba0095b3d1d41bca8a2a
robynp108 <robynp108@gmail.com>
2020-04-29 11:46:49 -0500

    edits after linter

--- public/js/savedRecipes.js
+++ public/js/savedRecipes.js
@@ -1,37 +1,30 @@
-
 //Search flow
-    //User inputs ingredients
-    //Call to api with ingredients in query
-    //Will get object
-    //Return res.data.hits.?
+//User inputs ingredients
+//Call to api with ingredients in query
+//Will get object
+//Return res.data.hits.?
 
-
-
-
-$(document).ready(function () {
-
-    //create a click handler on the /is-small class that 
-    //saves the recepie 
-    //gets all saved recipeies
-    $(".recSavBtn").on("click", function (event) {
-        console.log("clicked");
-        //save recipe
-        saveRecipe();
-        //get all saved
-        getAllRecipes();
-    })
+$(document).ready(function() {
+  //create a click handler on the /is-small class that
+  //saves the recepie
+  //gets all saved recipeies
+  $(".recSavBtn").on("click", function() {
+    console.log("clicked");
+    //save recipe
+    saveRecipe();
+    //get all saved
+    getAllRecipes();
+  });
 });
 
 function saveRecipe() {
-    $.post("/api/recipes").then(function (data) {
-        return data;
-    })
+  $.post("/api/recipes").then(function(data) {
+    return data;
+  });
 }
 
 function getAllRecipes() {
-    $.get("/api/recipes/selected").then(function (data) {
-        $(".recipe-title").append(data);
-    });
+  $.get("/api/recipes/selected").then(function(data) {
+    $(".recipe-title").append(data);
+  });
 }
-
-
