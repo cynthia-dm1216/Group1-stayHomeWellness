7120f32fc802bc693e84bfef0c241c899139b795
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-01 17:50:59 -0500

    testing'

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,7 +8,7 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
+  // Otherwise the user will be sent an error.
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
